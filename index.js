1df2568cf78bbacd60401aba58eb8c63c86abf47
Richie Bendall <richiebendall@gmail.com>
2019-11-02 23:00:00 +1300

    refactor: Use lodash for concatenation

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const _ = require("lodash")
 
 module.exports = (obj, ...sources) => _.mergeWith(obj, ...sources, (objValue, srcValue) => {
-    if (_.isArray(objValue)) return objValue.concat(srcValue)
+    if (_.isArray(objValue)) return _.concat(objValue, srcValue)
 })
